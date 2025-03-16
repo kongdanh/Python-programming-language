88ce90f80c1acf46cec236471025b8f2e241917d
KongDanh <danhcong6205@gmail.com>
2025-03-17 01:00:34 +0700

    Thêm ghi chú = TV

--- src/main.py
+++ src/main.py
@@ -1,87 +1,93 @@
-#import the libary
-import pygame
+#Thêm thư viện để chạy các hàm cần thiết
+import pygame   #chạy chương trình
 import math
-import random
+import random   #sinh ngẫu nhiên các đối tượng
+import os       #dùng  để lấy đường dẫn từ thư mục các máy (riêng biệt)
 
-#initialization init -> pygame
+#khởi tạo pygame
 pygame.init()
 
-#size of frame ((width, height), option)
+#Độ lớn của cửa sổ width: rộng, height: cao và option: các điều khác tương tác với cửa sổ
+# RESIZABLE -> cho phép thay đổi độ lớn cửa sổ game
 screen = pygame.display.set_mode((500,600), pygame.RESIZABLE)
 
-#title
+#Tiêu đề
 pygame.display.set_caption("Đồ án Python")
 
-#icon for title 
-icon = pygame.image.load("C:/Users/danhc/Documents/K2N2/PYTHON/DO_AN/assets/icon_screen.jpg")
+#Đường dẫn cố định
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+#icon cho tiêu đề
+icon = pygame.image.load(os.path.join(base_dir, "..", "assets", "icon_screen.jpg"))
 pygame.display.set_icon(icon)
 
-#background
-background = pygame.image.load("C:/Users/danhc/Documents/K2N2/PYTHON/DO_AN/assets/icon_screen.jpg")
+#Hình nền
+background = pygame.image.load(os.path.join(base_dir, "..", "assets", "icon_screen.jpg"))
 
-#editing background -> get width & height at screen size
+#Chỉnh sửa độ lớn hình nền cho khớp với độ lớn cửa sổ
 resized_background = pygame.transform.scale(background, (500, 600))
 
 #variable
-cols, rows = 5, 8
-square_size = 30
-spacing = 5
+cols, rows = 5, 8   #Số lượng hàng, cột
+square_size = 30    #Độ lớn 1 khối
+spacing = 5         #Khoảng cách giữa các khối
 
-moving_square = []
-speed = 0.01
+moving_square = []  #...
+speed = 0.01        #...(2)
 
-dragging = None
+dragging = None     #Khối hiện tại
 
-#current location
+#vị trí hiện tại
 original_pos = None
 
-#current cursor position
+#vị trí trỏ chuột mở đầu 0,0 
 offset_x, offset_y = 0,0
 
 #list
 squares = []
 image_list = []
 
-#path
-image_file =["C:/Users/danhc/Documents/K2N2/PYTHON/DO_AN/assets/img1.jpg",
-             "C:/Users/danhc/Documents/K2N2/PYTHON/DO_AN/assets/img2.jpg",
-             "C:/Users/danhc/Documents/K2N2/PYTHON/DO_AN/assets/img3.jpg",
-             "C:/Users/danhc/Documents/K2N2/PYTHON/DO_AN/assets/img4.jpg"]
+#Mảng đường dẫn tới hình ảnh
+image_file =[os.path.join(base_dir, "..", "assets", "img1.jpg"),
+             os.path.join(base_dir, "..", "assets", "img2.jpg"),
+             os.path.join(base_dir, "..", "assets", "img3.jpg"),
+             os.path.join(base_dir, "..", "assets", "img4.jpg")]
 
-#input image
+#Thêm hình ảnh từ mảng đường dẫn vào mảng hình ảnh và độ lớn = độ lớn khối
 for img_f in image_file:
     img = pygame.image.load(img_f)
     img = pygame.transform.scale(img, (square_size, square_size))
     image_list.append(img)
 
-#edit frames and obj as they change width & height
+#Cập nhật lại hình ảnh khung hình nếu có thay đổi về độ lớn cửa sổ
 def update_grid_position(width, height):
     
-    #create new list
+    #....(3)
     squares.clear()
     
-    #variable -> center screen
+    #Lấy thông số giữa cửa sổ
     grid_width = cols * square_size + (cols - 1) * spacing
     grid_height = rows * square_size + (rows - 1) * spacing
     start_x = (width - grid_width) //2
     start_y = (height - grid_height) //2
     
-    #content initialization
+    #Tạo các khối
     for row in range(rows):
         for col in range(cols):
             x = start_x + col * (square_size + spacing)
             y = start_y + row * (square_size + spacing)
             
-            #component of squares (obj list)
+            #Thêm các khối vào mảng (gồm các thông tin cụ thể)
             squares.append({"rect": pygame.Rect(x,y, square_size, square_size),
                             "pos": (col, row),
                             "image": random.choice(image_list)
                             })
 
-#original frame
+#độ lớn cửa sổ mặc định
 update_grid_position(500,600)
 
-#function changed location
+#... sẽ sửa đổi ...
 def find_nearest_square(target, start_pos, end_pos):
     
     direction = pygame.Vector2(end_pos) - pygame.Vector2(start_pos)
@@ -122,62 +128,63 @@ def find_nearest_square(target, start_pos, end_pos):
     
     # return nearest_square
 
+#hàm nhận khối được nhấn -> trả về khối được nhấn
 def get_square_clicked(target):
     clicked_square = []
     
     
     return 
 
-#start the program
+#khởi chạy chương trình
 running = True
 while running:
     
-    #draw background at (0,0)
+    #vẽ hình nền 
     screen.blit(resized_background,(0,0))
     
-    #get event from user
+    #lấy sự kiện từ người dùng
     for event in pygame.event.get():
         
-        #click on x
+        #khi nhấn nút x...
         if event.type == pygame.QUIT:
             running = False
         
-        #stretch frame
+        #thay đổi độ lớn cửa sổ
         if event.type == pygame.VIDEORESIZE:
             
-            #get new width & height
+            #lấy w, h mới
             width, height = event.w, event.h
             
-            #create new frame size
+            #tạo lại cửa sổ, đang bị lỗi tạo lại cả các khối
             screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
             resized_background = pygame.transform.scale(background, (width, height))
             update_grid_position(width, height)
         
-        #when user click
+        #khi người dùng nhấn
         if event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = event.pos
             for square in squares:
                 if square["rect"].collidepoint(mx,my):
                     
-                    #get current location, square
+                    #lấy địa chỉ hiện tại của trỏ chuột
                     dragging = square
                     original_pos = square["rect"].topleft
                     offset_x = mx - square["rect"].x
                     offset_y = my - square["rect"].y
                     break
         
-        #user move mouse
+        #khi người dùng di chuyển chuột
         if event.type == pygame.MOUSEMOTION and dragging:
             mx, my = event.pos
             # dragging["rect"].x = mx - offset_x
             # dragging["rect"].y = my - offset_y
         
-        #user release the mouse button
+        #sự kiện thả chuột
         if event.type == pygame.MOUSEBUTTONUP and dragging:
             
-            #call function
+            #gọi hàm ... sẽ sửa
             nearest_square = find_nearest_square(dragging,original_pos, dragging["rect"].topleft)
-            square_choice = get_square_clicked(target)
+            #square_choice = get_square_clicked(target)
             
             if nearest_square:
                 
@@ -208,7 +215,7 @@ while running:
 
             dragging = None
     
-    #
+    #animation chuyển động giữa các khối -> sẽ thay đổi bằng di chuyển khối vào ô chứa
     for move in moving_square[:]:
         square = move["square"]
         start = move["start_pos"]
@@ -223,13 +230,13 @@ while running:
         else:
             move["start_pos"] = new_pos
     
-    #create content
+    #tạo các khối mặc định
     for square in squares:
         pygame.draw.rect(screen, (255,255,255), square["rect"])
         screen.blit(square["image"], square["rect"].topleft)
     
-    # oval = pygame.draw.rect(screen, (255, 255, 255), (400, 300), 100)
-    # screen.blit(oval,(300,200))
+    rect = pygame.draw.rect(screen, (255, 255, 255), (50, 550, 400, 50))  # (x, y, width, height)
+    
     pygame.display.update()
     
 pygame.quit()
